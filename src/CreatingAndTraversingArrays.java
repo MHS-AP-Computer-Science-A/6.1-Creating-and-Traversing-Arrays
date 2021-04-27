1ae06dc46774dbd86840414c16e260242def01fd
foxrw1 <78601276+foxrw1@users.noreply.github.com>
2021-04-27 12:58:39 -0400

    Add files via upload

---
+++ src/CreatingAndTraversingArrays.java
@@ -0,0 +1,14 @@
+
+public class CreatingAndTraversingArrays {
+	public static void main(String[] args) {
+		//Create an array with an initializer list
+		int[] arr = {1, 2, 3, 4};
+				
+		//Print the array with a traditional for-loop
+		for(int i = 0; i < arr.length; i++){
+			System.out.print(arr[i] + " ");
+		}
+		System.out.println();
+	}
+		
+}
